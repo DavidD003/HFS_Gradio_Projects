f092e4fc70ac4270aa3eb82507dcc452e6acdd50
DavidD003 <53955537+DavidD003@users.noreply.github.com>
2022-09-05 11:36:40 -0400

    Building out the preassignment reader function.

--- Wknd_Sched_Builder/SchedBuilderClasses.py
+++ Wknd_Sched_Builder/SchedBuilderClasses.py
@@ -12,6 +12,11 @@ class Slot():
         self.slotInShift=0 #1 if first slot in someones shift. 2 if second, etc.
         self.totSlotsInShift=0 # 1 if 4 hours shift, 2 if 8 hour shift, 3 if 12 hour shift
         self.eligVol=0 #This will be used to track which slot is the most constrained...it tracks the count of how many people eligible volunteers this slot has going for it
+    def assn(self,assnType=None,slAssignee=None):
+        self.assnType=assnType
+        self.assignee=slAssignee
+        if slAssignee is not None:
+            ee
 
 class ee():
     """A staff persons data as related to weekend scheduling"""
@@ -35,18 +40,23 @@ class Schedule():
     def __init__(self,slots,ee,preAssn,senList,polling):
         self.slots= slots  #A collection of Slot objects that compose this schedule
         self.oslots= deepcopy(slots)
-        self.ee=ee
-        self.preAssn=preAssn
+        self.ee=ee #A dictionary containing ee info
+        self.preAssn=preAssn #A list of lists containing the predefind assignment info
         self.senList=senList
         self.polling=polling
 
     def evalAssnList(self):
         """Enter all predefined assignments into the schedule"""
+        #First, iterate through the assignment list. Each record will generate one or more records for the 'slot change log'
+        #The slot change log will have one record for each slot for which an assignment (or other specified status change) should be made
+        #So assignment log records that indicate a span of multiple slots will generate multiple records in the change log.
+        slChLg=[] #Initialize slot change log. It will be a list of lists where each sublist has the necessary info within to pass to the slot assignment function
         for myAssn in self.preAssn:
             if myAssn[0]==1: #Only evaluate those with '1' in 'Active' (first) column
-                if myAssn[5]=="" or myAssn[5]==None: #If no job assigned, then this is likely a DNS determination. Assign to all jobs in slot.
-                    myKeys=[]
-                    for seqNo in range(myAssn[2],myAssn[3]+1):#Apply to all slots in given range.. +1 due to range fn not being inclusive
-                        plus=[k for k in self.slots.keys() if k[:len(str(seqNo))+1]==str(seqNo)+'_'] #Pull dict keys for SLots where it is a slot with matching seqNo, regardless of job name
-                        myKeys.extend(plus)
-                    return myKeys
+                if myAssn[1]=='DNS':#Case of 'Do Not Schedule' modification to a slot
+                    if (myAssn[5]=="" or myAssn[5]==None): #Case of job not being specified
+                        myKeys=[] #This will collect the keys corresponding to all the slots in the slots dictionary to which this DNS will apply
+                        for seqNo in range(myAssn[2],myAssn[3]+1):#Apply to all slots in given range.. +1 due to range fn not being inclusive
+                            plus=[k for k in self.slots.keys() if k[:len(str(seqNo))+1]==str(seqNo)+'_'] #Pull dict keys for Slots where it is a slot with matching seqNo, regardless of job name
+                            myKeys.extend(plus)
+                        return myKeys
