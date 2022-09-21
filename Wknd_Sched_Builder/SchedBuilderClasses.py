703f964c535eef9ce217654b84785d5250bc550c
DavidD003 <53955537+DavidD003@users.noreply.github.com>
2022-09-20 21:44:28 -0400

    updated requirements

--- Wknd_Sched_Builder/SchedBuilderClasses.py
+++ Wknd_Sched_Builder/SchedBuilderClasses.py
@@ -1,22 +1,21 @@
 from copy import deepcopy
 import openpyxl as pyxl
-import functools
+# import functools
 import SchedBuilderUtyModule as tls
-from importlib import reload
-reload(tls)
+# from importlib import reload
 
-def debug(func):
-    """Print the function signature and return value"""
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]                      # 1
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)           # 3
-        print(f"Calling {func.__name__}({signature})")
-        value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value!r}")           # 4
-        return value
-    return wrapper_debug
+# def debug(func):
+#     """Print the function signature and return value"""
+#     @functools.wraps(func)
+#     def wrapper_debug(*args, **kwargs):
+#         args_repr = [repr(a) for a in args]                      # 1
+#         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+#         signature = ", ".join(args_repr + kwargs_repr)           # 3
+#         print(f"Calling {func.__name__}({signature})")
+#         value = func(*args, **kwargs)
+#         print(f"{func.__name__!r} returned {value!r}")           # 4
+#         return value
+#     return wrapper_debug
 
 
 class Slot():
--- Wknd_Sched_Builder/SchedBuilderUtyModule.py
+++ Wknd_Sched_Builder/SchedBuilderUtyModule.py
@@ -3,23 +3,20 @@ import openpyxl as pyxl
 import pandas as pd
 import numpy as np
 import sqlite3
-from copy import deepcopy
-from functools import reduce
-from operator import concat
 
 
-def debug(func):
-    """Print the function signature and return value"""
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]                      # 1
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)           # 3
-        print(f"Calling {func.__name__}({signature})")
-        value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value!r}")           # 4
-        return value
-    return wrapper_debug
+# def debug(func):
+#     """Print the function signature and return value"""
+#     @functools.wraps(func)
+#     def wrapper_debug(*args, **kwargs):
+#         args_repr = [repr(a) for a in args]                      # 1
+#         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+#         signature = ", ".join(args_repr + kwargs_repr)           # 3
+#         print(f"Calling {func.__name__}({signature})")
+#         value = func(*args, **kwargs)
+#         print(f"{func.__name__!r} returned {value!r}")           # 4
+#         return value
+#     return wrapper_debug
 
 
 
